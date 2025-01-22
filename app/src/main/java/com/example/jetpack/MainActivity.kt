a5dd0ca859fc07e9c2e5c66fb9ab49bdd544744d
Vallabha <vallabha1243@gmail.com>
2025-01-22 21:40:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack.ui.theme.JetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Hello CVR", modifier = modifier)
+        Spacer(modifier = Modifier.height(8.dp)) // Add spacing
+        Text(text = "Namashellote", modifier = modifier)
+        Spacer(modifier = Modifier.height(16.dp)) // Add more spacing
+        Button(onClick = {}) {
+            Text(text = "Button")
+        }
+        Spacer(modifier = Modifier.height(8.dp)) // Add spacing
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackTheme {
+        Greeting( "Android")
+    }
+}
